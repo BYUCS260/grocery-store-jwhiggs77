b13bd250bad0e8c578d56d633554ed747931d1f2
Joshua Higgins <jwhiggs77@gmail.com>
2022-03-07 14:32:51 -0700

    prepared to upload

---
+++ grocery-store/src/views/Cart.vue
@@ -0,0 +1,39 @@
+<template>
+<div>
+  <h1>Your Cart</h1>
+  <CartList :products="cart" />
+  <h2 class="emptyNote" v-if="this.$root.$data.cart.length == 0">Cart is empty</h2>
+</div>
+</template>
+
+<script>
+import CartList from "../components/CartList.vue"
+export default {
+  name: 'BrowsePage',
+  components: {
+    CartList
+  },
+  data() {
+    return {
+      
+    }
+  },
+  computed: {
+    cart() {
+      return this.$root.$data.cart;
+    }
+  },
+  methods: {
+    
+  }
+}
+</script>
+
+
+<style scoped>
+.emptyNote {
+  text-align: center;
+}
+
+
+</style>
